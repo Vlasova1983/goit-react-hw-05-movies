f41cae46add5cfa92b6e2ed4edc94c767eda24a2
Vlasova1983 <106443520+Vlasova1983@users.noreply.github.com>
2023-01-21 21:36:05 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,22 +1,28 @@
+import {lazy,Suspense} from 'react';
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
-import {Home} from './Home/Home';
-import {Movies} from './Movies/Movies';
-import {MoviesDetails} from './MoviesDetails/MoviesDetails';
-import { Layout } from './Layout/Layout';
+import { Loader } from './Loader/Loader';
+
+const Home = lazy(()=> import('./Home/Home'));
+const Movies = lazy(()=> import('./Movies/Movies'));
+const MoviesDetails = lazy(()=> import('./MoviesDetails/MoviesDetails'));
+const Layout = lazy(()=> import('./Layout/Layout'));
+
 
 export const App = () => {
  return(
-  <BrowserRouter basename="goit-react-hw-05-movies">
-    < Layout >
-      <Routes>    
-        <Route path="" element={<Home/>} />
-        <Route path="movies" element={<Movies />} />    
-        <Route path="movies/:movieId" element={<MoviesDetails/>}> 
-          <Route path="cast" element={<MoviesDetails/>} />
-          <Route path="reviews" element={<MoviesDetails/>}/>
-        </Route>      
-      </Routes> 
-    </Layout>    
+  <BrowserRouter basename="goit-react-hw-05-movies">    
+    <Suspense fallback={<Loader/>}>
+      < Layout >
+        <Routes>    
+          <Route path="" element={<Home/>} />
+          <Route path="movies" element={<Movies />} />    
+          <Route path="movies/:movieId" element={<MoviesDetails/>}> 
+            <Route path="cast" element={<MoviesDetails/>} />
+            <Route path="reviews" element={<MoviesDetails/>}/>
+          </Route>      
+        </Routes>
+      </Layout> 
+    </Suspense>        
   </BrowserRouter>
  );
 };
