2e5f0ccfbff67a4dbb1068a0278278056e5a361b
Vlasova1983 <106443520+Vlasova1983@users.noreply.github.com>
2023-01-24 11:03:22 +0200

    Update Home.jsx

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,34 +1,42 @@
 import styles  from '../Home/Home.module.css';
-import {   Link,useLocation } from 'react-router-dom';
-import {useState, useEffect} from 'react';
+
+import { Link,useLocation } from 'react-router-dom';
+import {useState, useEffect, useContext} from 'react';
+
 import Notiflix from 'notiflix';
 import axios from 'axios';
+
+import { FeatchContext } from 'context/FetchContext';
+
 
 const Home = () => {    
     const [movies,setIsMovies] = useState([]);
     const location = useLocation();
+    const featchContext = useContext(FeatchContext);   
+    const {url,key} = featchContext;
+    
 
     useEffect(()=>{
+        const fetchData = async () => {        
+            try {           
+                const response= await axios.get(`${url}/trending/movie/day?${key}`);
+                const data=response.data.results;                         
+                setIsMovies(data);                      
+            }
+            catch (error) {
+                Notiflix.Notify.failure('Sorry, your search did not match any results. Try again.');
+            } 
+                  
+        };
         fetchData();
-    },[])      
-    const fetchData = async () => {        
-        try {           
-            const response= await axios.get(`https://api.themoviedb.org/3/trending/movie/day?api_key=dbc34002be87151e0df6d0e75806eaf7`);
-            const data=response.data.results;                         
-            setIsMovies(data);                      
-        }
-        catch (error) {
-            Notiflix.Notify.failure('Sorry, your search did not match any results. Try again.');
-        } 
-        finally{
-        }               
-    };
+    },[url,key])    
 
     const fetchLink=(item)=>{            
         return(            
             <Link state={{from:location}} to= {`movies/${item.id}`}  className={styles.link_movies} key={item.id} id={item.id}> - {item.title}</Link>
         )         
     };
+   
 
     return (
         <>                
