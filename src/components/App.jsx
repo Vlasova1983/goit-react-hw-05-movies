2e560da28e005d210497f46e83ea6d477a812478
Vlasova1983 <106443520+Vlasova1983@users.noreply.github.com>
2023-01-24 11:03:18 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,6 +1,8 @@
 import {lazy,Suspense} from 'react';
 import { BrowserRouter, Routes, Route} from 'react-router-dom';
+
 import { Loader } from './Loader/Loader';
+import { FeatchContext } from 'context/FetchContext';
 
 const Home = lazy(()=> import('./Home/Home'));
 const Movies = lazy(()=> import('./Movies/Movies'));
@@ -14,14 +16,16 @@ export const App = () => {
   <BrowserRouter basename="goit-react-hw-05-movies">    
     <Suspense fallback={<Loader/>}>
       < Layout >
-        <Routes>    
-          <Route path="" element={<Home/>} />
-          <Route path="movies" element={<Movies />} />    
-          <Route path="movies/:movieId" element={<MoviesDetails/>}> 
-            <Route path="cast" element={<Cast/>} />
-            <Route path="reviews" element={<Reviews/>}/>
-          </Route>      
-        </Routes>
+        <FeatchContext.Provider value={{url:'https://api.themoviedb.org/3',key:'api_key=dbc34002be87151e0df6d0e75806eaf7' }}>
+          <Routes>    
+            <Route path="" element={<Home/>} />
+            <Route path="movies" element={<Movies />} />    
+            <Route path="movies/:movieId" element={<MoviesDetails/>}> 
+              <Route path="cast" element={<Cast/>} />
+              <Route path="reviews" element={<Reviews/>}/>
+            </Route>      
+          </Routes>
+        </FeatchContext.Provider>
       </Layout> 
     </Suspense>        
   </BrowserRouter>
